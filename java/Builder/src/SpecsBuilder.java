c0b56f05001aa4c5c3708ef2bc4a36f066b90e22
unknown <magbanua.c.ariel@gmail.com>
2021-09-04 13:34:07 +0800

    Renamed and cleaned up.

---
+++ java/Builder/src/SpecsBuilder.java
@@ -0,0 +1,22 @@
+/**
+ * Created by magbanua-ariel on 14/01/2016.
+ */
+public class SpecsBuilder {
+    public static void main(String[] args) {
+        // Get all the data for a newly acquired car.
+        // Code uses only predefined data but ideally all data should come from appropriate form screen
+        String name = "Lamborgini Gallardo";
+        String modelName = "SLG";
+        String manufacturer = "Lamborgini";
+        String engineName = "LV-1000";
+        String fuelType = "Diesel";
+        int horsePower = 0;
+        String wheelDiameter = "";
+        String wheelThickness = "";
+        int numberOfSeats = 1;
+        double weightLimit = 0;
+    }
+
+    public void addCarSpecsToCollection(Car car) {}
+
+}
