75528a0f0a98755ed421e900ba54a1f75b3e017b
unknown <magbanua.c.ariel@gmail.com>
2021-09-15 22:44:14 +0800

    Refactored.

--- behavioral/java/memento/src/examples/editor/History.java
+++ behavioral/java/memento/src/examples/editor/History.java
@@ -1,12 +1,10 @@
 package examples.editor;
 
-import examples.editor.EditorState;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class History {
-    private List<EditorState> states = new ArrayList<>();
+    private final List<EditorState> states = new ArrayList<>();
 
     public void push(EditorState state) {
         states.add(state);
