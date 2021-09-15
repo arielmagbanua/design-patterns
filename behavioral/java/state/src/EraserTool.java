e0a66d0f0a98570e86b7d08c7ee1b0cf368b4b3a
unknown <magbanua.c.ariel@gmail.com>
2021-09-15 22:29:35 +0800

    Moved state to correct directory.

---
+++ behavioral/java/state/src/EraserTool.java
@@ -0,0 +1,11 @@
+public class EraserTool implements Tool {
+    @Override
+    public void mouseDown() {
+        System.out.println("Brush icon");
+    }
+
+    @Override
+    public void mouseUp() {
+        System.out.println("Erase seomthing");
+    }
+}
