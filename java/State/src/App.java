b08828b551922b54d72d2b83c5dd25f486152d9b
unknown <magbanua.c.ariel@gmail.com>
2021-09-15 22:31:30 +0800

    Removed the old.

--- java/State/src/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        var canvas = new Canvas();
-        canvas.setCurrentTool(new SelectionTool());
-        canvas.mouseDown();
-        canvas.mouseUp();
-    }
-}
