dcc424ccf7d0e1fadf61a1d90e670b41edd61cc6
unknown <magbanua.c.ariel@gmail.com>
2021-09-19 19:15:08 +0800

    Changed to array iterator.

--- behavioral/java/iterator/src/examples/browser/BrowserHistory.java
+++ behavioral/java/iterator/src/examples/browser/BrowserHistory.java
@@ -4,40 +4,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BrowserHistory {
-    private final List<String> urls = new ArrayList<>();
+    private final String[] urls = new String[10];
+    private int count;
 
     public void push(String url) {
-        urls.add(url);
+        urls[count++] = url;
     }
 
     public String pop() {
-        var lastIndex = urls.size() - 1;
-        var lastUrl = urls.get(lastIndex);
-        urls.remove(lastUrl);
-
-        return lastUrl;
+        return urls[--count];
     }
 
     public Iterator<String> createIterator() {
-        return new ListIterator(this);
+        return new ArrayIterator(this);
     }
 
-    private static class ListIterator implements Iterator<String> {
+    private static class ArrayIterator implements Iterator<String> {
         private final BrowserHistory history;
         private int index;
 
-        public ListIterator(BrowserHistory history) {
+        public ArrayIterator(BrowserHistory history) {
             this.history = history;
         }
 
         @Override
         public boolean hasNext() {
-            return index < history.urls.size();
+            return index < history.count;
         }
 
         @Override
         public String current() {
-            return history.urls.get(index);
+            return history.urls[index];
         }
 
         @Override
