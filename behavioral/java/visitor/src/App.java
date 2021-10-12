7e91bb501ab304c2281c61c4680db1d235aecde6
unknown <magbanua.c.ariel@gmail.com>
2021-10-12 23:56:10 +0800

    Added new example

--- behavioral/java/visitor/src/App.java
+++ behavioral/java/visitor/src/App.java
@@ -1,4 +1,8 @@
 import examples.html.*;
+import examples.wav.NoiseReductionFilter;
+import examples.wav.NormalizeFilter;
+import examples.wav.ReverbFilter;
+import examples.wav.WavFile;
 
 public class App {
     public static void main(String[] args) {
@@ -7,5 +11,10 @@ public class App {
         document.add(new AnchorNode());
         document.execute(new HighlightOperation());
         document.execute(new PlainTextOperation());
+
+        var wavFile = WavFile.read("myfile.wav");
+        wavFile.applyFilter(new NoiseReductionFilter());
+        wavFile.applyFilter(new ReverbFilter());
+        wavFile.applyFilter(new NormalizeFilter());
     }
 }
---
+++ behavioral/java/visitor/src/examples/wav/FactSegment.java
@@ -0,0 +1,8 @@
+package examples.wav;
+
+public class FactSegment extends Segment {
+    @Override
+    public void applyFilter(AudioFilter filter) {
+        filter.apply(this);
+    }
+}
